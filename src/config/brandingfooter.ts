2accb3395c9864f17c987fe9a54a02d2a849cfb4
mals252 <tsaqifulilmi56@gmmail.com>
2026-02-04 14:00:10 +0700

    update

--- src/config/brandingfooter.ts
+++ src/config/brandingfooter.ts
@@ -1,7 +1,7 @@
 export const branding = {
     name: "KKN Kelompok 10",
     description: "Desa Gondowangi",
-    footerDescription: "Kami melaksanakan revitalisasi Ponkesdes untuk meningkatkan fasilitas kesehatan desa serta pemasangan neon box guna memperjelas informasi dan identitas layanan publik.",
+    footerDescription: "1Kami melaksanakan revitalisasi Ponkesdes untuk meningkatkan fasilitas kesehatan desa serta pemasangan neon box guna memperjelas informasi dan identitas layanan publik.",
     whatsapp: "628815520154",
     footerLogo: {
         type: "image" as "icon" | "image",
